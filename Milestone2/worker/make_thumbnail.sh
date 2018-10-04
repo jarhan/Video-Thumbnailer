9ce91771fcd2f19c5e2f1993d6c428eed2488504
Jarosporn Hansuekkaew <jarhan.jh@gmail.com>
2018-10-04 21:39:56 +0700

    minor fix worker

--- Milestone2/worker/make_thumbnail.sh
+++ Milestone2/worker/make_thumbnail.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-
-bucket_name="$1"
-object_name="$2"
+worker_name="$1"
+bucket_name="$2"
+object_name="$3"
 min_w=320
 min_h=240
 
 echo $bucket_name
 echo $object_name
 
-mkdir resources/frames/${bucket_name}
-mkdir resources/frames/${bucket_name}/${object_name}
-mkdir resources/gif/${bucket_name}
+mkdir resources/frames/${worker_name}
+mkdir resources/frames/${worker_name}/${bucket_name}
+mkdir resources/frames/${worker_name}/${bucket_name}/${object_name}
+
+mkdir resources/gif/${worker_name}
+mkdir resources/gif/${worker_name}/${bucket_name}
 
 # extract frames from video
-ffmpeg -ss 00:00:00 -i resources/video/$bucket_name/$object_name -vframes 20 -filter:v fps=fps=1/2 resources/frames/${bucket_name}/${object_name}/frame_%02d.bmp
+ffmpeg -ss 00:00:00 -i resources/video/${worker_name}/$bucket_name/$object_name -vframes 20 -filter:v fps=fps=1/2 resources/frames/${worker_name}/${bucket_name}/${object_name}/frame_%02d.bmp
 
 # get image size
-image_widht=$(convert resources/frames/${bucket_name}/${object_name}/frame_01.bmp -print "%wx%h" /dev/null | cut -d'x' -f1)
-image_height=$(convert resources/frames/${bucket_name}/${object_name}/frame_01.bmp -print "%wx%h" /dev/null | cut -d'x' -f2)
+image_widht=$(convert resources/frames/${worker_name}/${bucket_name}/${object_name}/frame_01.bmp -print "%wx%h" /dev/null | cut -d'x' -f1)
+image_height=$(convert resources/frames/${worker_name}/${bucket_name}/${object_name}/frame_01.bmp -print "%wx%h" /dev/null | cut -d'x' -f2)
 
 # convert frame size for no smaller than 320x240
 if [[ $image_height -lt $min_h ]]; then
@@ -35,9 +38,9 @@ gif_size=${image_widht}x${image_height}!
 echo $gif_size
 
 # generate .gif
-convert -resize $gif_size -delay 30 -loop 0 resources/frames/${bucket_name}/${object_name}/frame_*.bmp resources/gif/${bucket_name}/${object_name}.gif
+convert -resize $gif_size -delay 30 -loop 0 resources/frames/${worker_name}/${bucket_name}/${object_name}/frame_*.bmp resources/gif/${worker_name}/${bucket_name}/${object_name}.gif
 
 # delete created frames
-rm -f resources/frames/${bucket_name}/${object_name}/frame_*.bmp
+rm -f resources/frames/${worker_name}/${bucket_name}/${object_name}/frame_*.bmp
 
 echo "done"
